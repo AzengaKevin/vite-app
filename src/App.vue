f0439c7b100ec547aaf57ea102e243518c3780f1
Azenga Kevin <azenga.kevin7@gmail.com>
2022-06-30 13:35:39 +0300

    WIP: Changed the font and the base color

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="bg-light">
-        <header class="shadow-sm">
-            <nav class="navbar navbar-expand-md bg-white">
-                <div class="container-fluid">
-                    <router-link class="navbar-brand" to="/">Vite App</router-link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <router-link class="nav-link" to="/about">About</router-link>
-                            </li>
-                        </ul>
-                    </div>
+    <header class="shadow-sm">
+        <nav class="navbar navbar-expand-md bg-white">
+            <div class="container-fluid">
+                <router-link class="navbar-brand" to="/">Vite App</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/about">About</router-link>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </header>
-        <router-view class="py-4" />
-    </div>
+            </div>
+        </nav>
+    </header>
+    <router-view class="py-4" />
 </template>
