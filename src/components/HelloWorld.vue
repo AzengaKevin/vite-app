f29d1956e213e71ae0737de0528ac22da8ef7c10
Azenga Kevin <azenga.kevin7@gmail.com>
2022-06-30 13:14:49 +0300

    WIP: Added and configured Pinia in the application

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import { useCounterStore } from '../stores/CounterStore';
 
 defineProps({
     msg: String,
 })
 
-const count = ref(0)
+const counterStore = useCounterStore()
 </script>
 
 <template>
@@ -13,8 +14,12 @@ const count = ref(0)
 
     <p class="text-muted">Just learning how to setup my Vue JS application in my supposed coming Vue JS application development career</p>
 
+    <div class="d-inline-flex border border-secondary rounded">
+        <button type="button" @click="counterStore.decrement" class="px-3 py-2 text-danger border-0 rounded-start">-</button>
+        <span class="d-inline-flex px-3 align-items-center">
+            <div class="align-middle">{{ counterStore.count }}</div>
+        </span>
+        <button type="button" @click="counterStore.increment" class="px-3 py-2 text-success border-0 rounded-end">+</button>
+    </div>
 
-    <button type="button" class="btn btn-lg btn-outline-primary" @click="count++">
-        Count: {{ count }}
-    </button>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import 'bootstrap'
 import './sass/app.scss'
 import router from './router'
 
 const app = createApp(App)
+
 app.use(router)
+
+// Cofigure Pinia
+const pinia = createPinia()
+app.use(pinia)
+
 app.mount('#app')
